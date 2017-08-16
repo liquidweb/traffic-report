955d72f4293aeb819f741647d4234b6290c242d5
Steve Grunwell <sgrunwell@liquidweb.com>
2017-08-16 19:39:17 +0000

    Clean scaffold of a new plugin, via WP-CLI. Initial commit.

---
+++ traffic-report.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Traffic Report
+ * Plugin URI:  https://github.com/liquidweb/traffic-report
+ * Description: Get details about your site's performance from right within WordPress
+ * Author:      Liquid Web
+ * Author URI:  https://www.liquidweb.com
+ * Text Domain: traffic-report
+ * Domain Path: /languages
+ * Version:     1.0.0
+ * License:     MIT
+ * License URI: https://opensource.org/licenses/MIT
+ *
+ * @package LiquidWeb\TrafficReport
+ * @author  Liquid Web
+ */
