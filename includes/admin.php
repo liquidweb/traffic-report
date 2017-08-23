c1ac458fb9b1de86eaececb6c8b87ea7fe08d34c
Steve Grunwell <sgrunwell@liquidweb.com>
2017-08-23 21:15:08 +0000

    Enable both the display of page views and the sorting of posts by page views

--- includes/admin.php
+++ includes/admin.php
@@ -22,6 +22,54 @@ function add_pageviews_column( $columns ) {
 add_filter( 'manage_posts_columns', __NAMESPACE__ . '\add_pageviews_column' );
 
 /**
+ * Enable posts to be sorted by page views.
+ *
+ * @param array $columns An array of sortable columns.
+ * @return array The filtered $columns array.
+ */
+function make_pageviews_column_sortable( $columns ) {
+	$columns['traffic-report'] = 'traffic-report';
+
+	return $columns;
+}
+add_filter( 'manage_edit-post_sortable_columns', __NAMESPACE__ . '\make_pageviews_column_sortable' );
+
+/**
+ * Handle sorting results by page views.
+ *
+ * @param WP_Query $query The current WP_Query instance, passed by reference.
+ */
+function maybe_sort_by_page_views( $query ) {
+	if ( ! is_admin() || ! $query->is_main_query() || 'traffic-report' !== $query->get( 'orderby' ) ) {
+		return;
+	}
+
+	$order = $query->get( 'order' );
+
+	$query->set( 'meta_query', array_merge(
+		(array) $query->get( 'meta_query' ),
+		array(
+			'relation'            => 'OR',
+			'traffic_report_meta' => array(
+				'key'     => '_traffic_report_views',
+				'compare' => 'EXISTS',
+				'type'    => 'numeric',
+			),
+			'traffic_report_none' => array(
+				'key'     => '_traffic_report_views',
+				'compare' => 'NOT EXISTS',
+				'type'    => 'numeric',
+			),
+		)
+	) );
+	$query->set( 'orderby', array(
+		'traffic_report_meta' => $order,
+		'traffic_report_none' => $order,
+	) );
+}
+add_action( 'pre_get_posts', __NAMESPACE__ . '\maybe_sort_by_page_views' );
+
+/**
  * Display the view count in the post column.
  *
  * @param string $column  The column being populated.
@@ -34,6 +82,8 @@ function populate_column( $column, $post_id ) {
 		return;
 	}
 
-	echo '123';
+	global $wp_query;
+
+	echo esc_html( get_post_meta( $post_id, '_traffic_report_views', true ) );
 }
 add_action( 'manage_posts_custom_column', __NAMESPACE__ . '\populate_column', 10, 2 );
--- tests/test-admin.php
+++ tests/test-admin.php
@@ -20,18 +20,79 @@ class AdminTest extends WP_UnitTestCase {
 		$contents = ob_get_clean();
 
 		$this->assertContains(
-			'<th scope="col" id=\'traffic-report\' class=\'manage-column column-traffic-report\'>',
+			'<th scope="col" id=\'traffic-report\'',
 			$contents,
 			'The Traffic Report column does not appear on the post list table.'
 		);
 	}
 
+	public function test_make_pageviews_column_sortable() {
+		$this->factory()->post->create();
+
+		$table = _get_list_table( 'WP_Posts_List_Table', [
+			'screen' => 'edit-post',
+		] );
+
+		$this->assertArrayHasKey( 'traffic-report', $table->get_column_info()[2] );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 */
+	public function test_can_sort_post_list_by_pageviews() {
+		$top_post = $this->factory()->post->create( [
+			'post_title' => 'A popular post',
+		]);
+		$sad_post = $this->factory()->post->create( [
+			'post_title' => 'A not-so-popular post',
+		]);
+		$new_post = $this->factory()->post->create( [
+			'post_title' => 'A brand new post',
+		]);
+
+		add_post_meta( $top_post, '_traffic_report_views', 100 );
+		add_post_meta( $sad_post, '_traffic_report_views', 2 );
+
+		set_current_screen( 'edit.php' );
+		$this->go_to( admin_url( 'edit.php?orderby=traffic-report&order=desc' ) );
+
+		global $wp_query;
+
+		$this->assertCount( 3, $wp_query->posts );
+		$this->assertEquals( $top_post, $wp_query->posts[0]->ID, 'Posts with high counts should be at the top.' );
+		$this->assertEquals( $sad_post, $wp_query->posts[1]->ID, 'Posts with low counts should be at the bottom.' );
+		$this->assertEquals( $new_post, $wp_query->posts[2]->ID, 'Posts without counts should be treated as 0.' );
+	}
+
+	public function test_can_sort_post_list_by_pageviews_in_ascending_order() {
+		$top_post = $this->factory()->post->create( [
+			'post_title' => 'A popular post',
+		]);
+		$sad_post = $this->factory()->post->create( [
+			'post_title' => 'A not-so-popular post',
+		]);
+		$new_post = $this->factory()->post->create( [
+			'post_title' => 'A brand new post',
+		]);
+
+		add_post_meta( $top_post, '_traffic_report_views', 100 );
+		add_post_meta( $sad_post, '_traffic_report_views', 2 );
+
+		set_current_screen( 'edit.php' );
+		$this->go_to( admin_url( 'edit.php?orderby=traffic-report&order=asc' ) );
+
+		global $wp_query;
+
+		$this->assertEquals( $top_post, $wp_query->posts[2]->ID, 'Posts with high counts should be at the bottom.' );
+		$this->assertEquals( $sad_post, $wp_query->posts[1]->ID, 'Posts with low counts should be at the top.' );
+		$this->assertEquals( $new_post, $wp_query->posts[0]->ID, 'Can\'t get any lower than 0, huh?' );
+	}
+
 	public function test_post_column_shows_page_view_count() {
-		$this->markTestSkipped( 'Table rows are not yet displaying real values.' );
 		$post  = $this->factory()->post->create_and_get();
 		$views = mt_rand( 0, 1500 );
 
-		add_post_meta( $post->ID, '_traffic_report_page_views', $views, true );
+		add_post_meta( $post->ID, '_traffic_report_views', $views, true );
 
 		$table = _get_list_table( 'WP_Posts_List_Table', [
 			'screen' => 'edit-post',
